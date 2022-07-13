ec6eb748b774755c14383c89295641591d8836aa
Frankie Lam <frankie191193@gmail.com>
2022-07-13 18:27:36 +0100

    replace Account NavActions to a svg

--- src/assets/Icons/index.js
+++ src/assets/Icons/index.js
@@ -4,3 +4,5 @@ export { BsArrowRightCircle as RightArrowIcon } from "react-icons/bs";
 export { BsFacebook as FacebookIcon } from "react-icons/bs";
 export { BsTwitter as TwitterIcon } from "react-icons/bs";
 export { AiOutlineInstagram as InstagramIcon } from "react-icons/ai";
+export { MdOutlineAccountCircle as AccountIcon } from "react-icons/md";
+export { BsCart as CartIcon } from "react-icons";
--- src/components/Header/HeaderNavActions.tsx
+++ src/components/Header/HeaderNavActions.tsx
@@ -1,11 +1,16 @@
 import styles from "./HeaderNavActions.module.css";
 import Link from "next/link";
 import Cart from "../Cart/Cart";
+import { AccountIcon } from "../../assets/Icons";
 
 const HeaderNavActions = () => {
   return (
     <div className={styles.actions}>
-      <Link href="/account">Account</Link>
+      <Link href="/account">
+        <a>
+          <AccountIcon />
+        </a>
+      </Link>
       <Cart />
     </div>
   );

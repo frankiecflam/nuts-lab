c28e8c7ef2a5b70a3654fe74454f66939075868e
Frankie Lam <frankie191193@gmail.com>
2022-07-15 10:52:15 +0100

    add modal-root and overlay-root to Main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,7 +6,13 @@ interface MainProps {
 }
 
 const Main: FC<MainProps> = ({ children }) => {
-  return <main className={styles.main}>{children}</main>;
+  return (
+    <main className={styles.main}>
+      <div id="overlay-root" />
+      <div id="modal-root" />
+      {children}
+    </main>
+  );
 };
 
 export default Main;

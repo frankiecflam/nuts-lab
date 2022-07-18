57a55da8fb4253d0588cc76ba891935be34b6541
Frankie Lam <frankie191193@gmail.com>
2022-07-18 12:07:48 +0100

    add rendering a message saying no cart items for checkout in CheckoutContent

--- src/components/Checkout/CheckoutContent.tsx
+++ src/components/Checkout/CheckoutContent.tsx
@@ -1,16 +1,37 @@
 import styles from "./CheckoutContent.module.css";
 import { Section, SectionHeader, SectionBody } from "../Section";
-import { Container } from "../UI/index";
+import { Button, Container } from "../UI/index";
 import { CheckoutShippingInfo, CheckoutOrderSummary } from "./index";
+import { useCartContext } from "../../context/CartContext";
+import Link from "next/link";
 
 const CheckoutContent = () => {
+  const { items } = useCartContext();
+  const isCartEmpty = items.length === 0;
+
   return (
     <Section className={styles.checkout}>
       <Container>
         <SectionHeader title="checkout" />
         <SectionBody className={styles.checkoutBody}>
-          <CheckoutShippingInfo />
-          <CheckoutOrderSummary />
+          {isCartEmpty && (
+            <div className={styles.checkoutCartEmpty}>
+              <p className={styles.checkoutCartEmptyMessage}>
+                No items are currently in your cart for checkout.
+              </p>
+              <Link href="/products">
+                <a>
+                  <Button name="view products" type="button" />
+                </a>
+              </Link>
+            </div>
+          )}
+          {!isCartEmpty && (
+            <>
+              <CheckoutShippingInfo />
+              <CheckoutOrderSummary />
+            </>
+          )}
         </SectionBody>
       </Container>
     </Section>

8e3c71973072ea33e7fec5ca1a9a313373ae8a58
Frankie Lam <frankie191193@gmail.com>
2022-11-03 09:43:33 +0000

    add zod to validate api response of products

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,20 +1,39 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Product } from "../types";
+import { z } from "zod";
+
+const Product = z.object({
+  id: z.string(),
+  title: z.string(),
+  description: z.string(),
+  price: z.number(),
+  image: z.string(),
+  topPick: z.boolean(),
+});
+const Products = z.array(Product);
 
 export default function useProducts() {
   return useQuery(
     ["products"],
-    () => axios.get("/api/products").then((res) => res.data),
-    {
-      select: ({ data }) => {
-        let loadedProducts: Product[] = [];
+    () =>
+      axios.get("/api/products").then((res) => {
+        const { products } = res.data;
 
-        for (const key in data) {
-          loadedProducts.push(data[key]);
+        let loadedProducts: any[] = [];
+
+        for (const key in products) {
+          loadedProducts.push(products[key]);
         }
 
-        return loadedProducts;
+        const validatedProducts = Products.parse(loadedProducts);
+
+        return validatedProducts;
+      }),
+    {
+      onError: (error) => {
+        console.log(
+          "Error occured when zod schema was parsing the api response of products!"
+        );
       },
     }
   );
